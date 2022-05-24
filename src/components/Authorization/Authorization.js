76a3a10aa3b1365eed9c307e597eee855e27ed1c
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-05-24 22:18:03 +0300

    completed page Authorization

--- src/components/Authorization/Authorization.js
+++ src/components/Authorization/Authorization.js
@@ -24,7 +24,7 @@ function Authorization({
           type="email"
           name={`${name}Email`}
         />
-        <span className={`${name}Email-input-error authorization__input-error`}></span>
+        <span className={`authorization__input-error ${name}Email-input-error`}></span>
         <input
           id={`${name}Password-input`}
           className={`authorization__input authorization__input_${name}-password`}
@@ -36,7 +36,7 @@ function Authorization({
           minLength="6"
           maxLength="12"
         />
-        <span className={`${name}Password-input-error authorization__input-error`}></span>
+        <span className={`authorization__input-error ${name}Password-input-error`}></span>
         <button
           className={`authorization__save-button`}
           type="submit"
@@ -45,7 +45,7 @@ function Authorization({
           {buttonText}
         </button>
       </form>
-      <p className="authorization__comment">Уже зарегистрированы?<a className="authorization__link-entry"> Войти</a></p>
+      <p className="authorization__comment">Уже зарегистрированы?<a className="authorization__link-entry" href="#" target="_blank"> Войти</a></p>
     </div>
   )
 }
